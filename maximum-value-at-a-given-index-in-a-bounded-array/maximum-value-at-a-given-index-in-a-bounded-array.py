359318fd5926872f227d92675a22a57fd889de94
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-06-10 21:55:53 +0500

    Time: 17 ms (77.78%) | Memory: 13.3 MB (88.89%) - LeetSync

--- maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -9,12 +9,13 @@ class Solution(object):
         if n == 1:
             return maxSum
             
-        temp1 = ((index)*(index + 1))//2
-        temp2 = ((n - index - 1)*(n - index))//2
+        temp1 = ((index) * (index + 1))//2
+        temp2 = ((n - index - 1) * (n - index))//2
         
         left = 1
         right = maxSum - (n - 1)
         x = (left + right)//2
+        
         while x >= 1:
             if x >= 1 + index:
                 left_sum = index * x
@@ -31,12 +32,16 @@ class Solution(object):
                 right_sum = n - index - x
                 temp = (x*(x - 1))//2
                 right_sum += temp
+            
             total_sum = left_sum + right_sum + x
+            
             if total_sum > maxSum:
                 right = x
             elif total_sum < maxSum:
                 left = x
+            
             prev = x
             x = (left + right)//2
+            
             if prev == x:
                 return x
