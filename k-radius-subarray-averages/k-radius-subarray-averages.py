3b1bfafad24abe7dbb3212957c68943cd01aaa0a
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-06-20 16:19:00 +0500

    Time: 1748 ms (8.77%) | Memory: 32.9 MB (92.98%) - LeetSync

---
+++ k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def getAverages(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        left_sum = 0
+        if k > len(nums) - 1:
+            right_sum = sum(nums[1:])
+        else:
+            right_sum = sum(nums[1:k + 1])
+        k_average = []
+        for i, num in enumerate(nums):
+            if i - k < 0 or i + k >= len(nums):
+                k_average.append(-1)
+            else:
+                k_average.append((left_sum + num + right_sum)//(2*k + 1))
+            if i - k >= 0:
+                left_sum -= nums[i - k]
+            left_sum += nums[i]
+            if i + k + 1 < len(nums):
+                right_sum += nums[i + k + 1]
+            if i + 1 < len(nums):
+                right_sum -= nums[i + 1]
+        return k_average
