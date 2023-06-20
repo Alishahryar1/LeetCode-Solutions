6065dae4cacc3e5f5814339d731ce3a046b95362
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-06-20 16:44:02 +0500

    Time: 1729 ms (10.53%) | Memory: 32.9 MB (92.98%) - LeetSync

--- k-radius-subarray-averages/k-radius-subarray-averages.py
+++ k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -10,17 +10,18 @@ class Solution(object):
             right_sum = sum(nums[1:])
         else:
             right_sum = sum(nums[1:k + 1])
-        k_average = []
+        k_average = [0] * len(nums)
         for i, num in enumerate(nums):
             if i - k < 0 or i + k >= len(nums):
-                k_average.append(-1)
+                k_average[i] = -1
             else:
-                k_average.append((left_sum + num + right_sum)//(2*k + 1))
+                k_average[i] = (left_sum + num + right_sum)//(2*k + 1)
             if i - k >= 0:
                 left_sum -= nums[i - k]
-            left_sum += nums[i]
+            left_sum += num
             if i + k + 1 < len(nums):
                 right_sum += nums[i + k + 1]
             if i + 1 < len(nums):
                 right_sum -= nums[i + 1]
+        
         return k_average
