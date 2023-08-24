dc61ce8370e6279b51a3b7bb51f4758c90d13cda
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-08-23 19:38:11 -0700

    Time: 1179 ms (39.14%) | Memory: 16.7 MB (41.94%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,33 +5,27 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        hashmap = {}
-
+        output = []
         for i in range(len(nums)):
-            target = -nums[i]
-            left = 0
-            right = len(nums) - 1
-            while left < right:
-                if left == i:
-                    left += 1
+            a = nums[i]
+            if i > 0 and a == nums[i - 1]:
+                continue
+            L = i + 1
+            R = len(nums) - 1
+            while L < R:
+                if L > i + 1 and nums[L] == nums[L - 1]:
+                    L += 1
                     continue
-                elif right == i:
-                    right -= 1
-                    continue
-                two_sum = nums[left] + nums[right]
-                if two_sum == target:
-                    if nums[i] < nums[left]:
-                        a = [nums[i], nums[left], nums[right]]
-                    elif nums[i] < nums[right]:
-                        a = [nums[left], nums[i],nums[right]]
-                    else:
-                        a = [nums[left], nums[right], nums[i]]
-                    hashmap[tuple(a)] = a
-                    left += 1
-                    right -= 1
-                elif two_sum < target:
-                    left += 1
+                b = nums[L]
+                c = nums[R]
+                total = a + b + c
+                if total == 0:
+                    output.append([a, b, c])
+                    L += 1
+                    R -= 1
+                elif total > 0:
+                    R -= 1
                 else:
-                    right -= 1
-
-        return list(hashmap.values())
+                    L += 1
+        
+        return output
