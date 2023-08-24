93baae42519122c839227d63a732d8cc68b95dc2
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-08-23 20:48:36 -0700

    Time: 51 ms (91.52%) | Memory: 25.7 MB (94.59%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -7,19 +7,23 @@
 class Solution(object):
     def __init__(self):
         self.max_sum = float('-inf')
+    
     def helper(self, root):
         if root.left == None and root.right == None:
             self.max_sum = max(self.max_sum, root.val)
             return root.val
+        
         if root.left != None and root.right != None:
             left_sum = self.helper(root.left)
             right_sum = self.helper(root.right)
             self.max_sum = max(self.max_sum, root.val + left_sum + right_sum, root.val, root.val + left_sum, root.val + right_sum)
             return max(root.val + left_sum, root.val + right_sum, root.val)
+        
         elif root.right != None:
             right_sum = self.helper(root.right)
             self.max_sum = max(self.max_sum, root.val + right_sum, root.val)
             return max(root.val + right_sum, root.val)
+        
         elif root.left != None:
             left_sum = self.helper(root.left)
             self.max_sum = max(self.max_sum, root.val + left_sum, root.val)
