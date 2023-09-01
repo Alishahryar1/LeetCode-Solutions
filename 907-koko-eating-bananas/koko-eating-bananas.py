7531253cb193f3a3e52418743b132cdc4ac32577
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-08-31 20:23:35 -0700

    Time: 458 ms (35.40%) | Memory: 14.6 MB (40.01%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,15 +1,9 @@
 class Solution(object):
-    def ceil(self, a, b):
-        return int(-1 * a // b * -1)
-
     def canFinish(self, k, h, piles):
         total_h = 0
         for pile in piles:
-            total_h += self.ceil(pile, k)
-            if total_h > h:
-                return False
-        
-        return True
+            total_h += math.ceil(pile/float(k))
+        return total_h <= h
 
     def minEatingSpeed(self, piles, h):
         """
@@ -19,7 +13,7 @@ class Solution(object):
         """
         L = 1
         R = max(piles)
-        min_k = float('inf')
+        min_k = R
         while L <= R:
             m = (L + R)//2
             canFinish = self.canFinish(m, h, piles)
