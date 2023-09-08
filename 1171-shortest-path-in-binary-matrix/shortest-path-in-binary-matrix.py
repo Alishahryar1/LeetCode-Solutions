85cddc1cdaf91d7c3f9a96b9ba338017a84e3eb6
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-07 22:23:24 -0700

    Time: 454 ms (84.27%) | Memory: 13.7 MB (79.49%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -8,20 +8,21 @@ class Solution(object):
         if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1 
         queue = deque()
-        queue.append((0, 0, 1))
+        queue.append((0, 0))
         grid[0][0] = 1
         length = 1
+        neighbours = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
         while queue:
-            r, c, length = queue.popleft()
-            if r == n - 1 and c == n - 1:
-                return length
-            neighbours = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-            for dr, dc in neighbours:
-                r_new = r + dr
-                c_new = c + dc
-                if min(r_new, c_new) < 0 or r_new == n or c_new == n or grid[r_new][c_new] == 1:
-                    continue
-                queue.append((r_new, c_new, length + 1))
-                grid[r_new][c_new] = 1
-
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                if r == n - 1 and c == n - 1:
+                    return length
+                for dr, dc in neighbours:
+                    r1, c1 = r + dr, c + dc
+                    if min(r1, c1) < 0 or r1 == n or c1 == n or grid[r1][c1] == 1:
+                        continue
+                    queue.append((r1, c1))
+                    grid[r1][c1] = 1
+            length += 1 
+        
         return -1
