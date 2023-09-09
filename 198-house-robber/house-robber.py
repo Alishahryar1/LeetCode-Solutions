bc12994a97a1e049b4c0f839b6546c7e528467df
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 20:07:20 -0700

    Time: 7 ms (98.71%) | Memory: 13.5 MB (8.54%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -7,7 +7,7 @@ class Solution(object):
         if len(nums) <= 2:
             return max(nums)
         
-        dp = [nums[0], max(nums[0], nums[1])]
+        a, b = nums[0], max(nums[:2])
         for i in range(2, len(nums)):
-            dp[0], dp[1] = dp[1], max(dp[1], dp[0] + nums[i])
-        return dp[-1]
+            a, b = b, max(a + nums[i], b)
+        return b
