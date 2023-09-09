3199422d77cb6292dacab313302bfcde21ab22e9
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 23:46:12 -0700

    Time: 789 ms (57.93%) | Memory: 13.2 MB (99.96%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -7,13 +7,13 @@ class Solution(object):
         """
         dp = [0] * (amount + 1)
         for coin in range(1, amount + 1):
-            dp[coin] = float('inf')
+            dp[coin] = amount + 1
             for c in coins:
                 if coin - c >= 0:
                     dp[coin] = min(dp[coin], dp[coin - c])
             dp[coin] += 1
         
-        if dp[-1] < float('inf'):
+        if dp[-1] <= amount:
             return dp[-1]
         else:
             return -1
