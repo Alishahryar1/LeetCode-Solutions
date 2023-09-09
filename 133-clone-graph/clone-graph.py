1feba764042f66016aa3793e66b94dc01376b163
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 18:43:50 -0700

    Time: 34 ms (55.52%) | Memory: 13.5 MB (92.94%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,10 +1,3 @@
-from collections import deque
-
-class Node(object):
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
 class Solution(object):
     def cloneGraph(self, node):
         """
