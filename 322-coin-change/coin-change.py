3a43926bd82b9dff9c1dc314930dd060495fbe79
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 23:40:02 -0700

    Time: 873 ms (47.46%) | Memory: 14.1 MB (29.52%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,28 +1,23 @@
 class Solution(object):
-    def helper(self, coins, amount, cache):
-        if amount < 0:
-            return float('inf')
-        if amount == 0:
-            return 0
-        if amount in coins:
-            return 1
-        if amount in cache:
-            return cache[amount]
-        
-        cache[amount] = float('inf')
-        for coin in coins:
-            cache[amount] = min(cache[amount], self.helper(coins, amount - coin, cache))
-        cache[amount] += 1
-        return cache[amount]
-
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
         :type amount: int
         :rtype: int
         """
-        ans = self.helper(coins, amount, {})
-        if ans < float('inf'):
-            return ans
+        dp = [0] * (amount + 1)
+        for coin in range(1, amount + 1):
+            if coin in coins:
+                dp[coin] = 1
+                continue
+            dp[coin] = float('inf')
+            for c in coins:
+                if coin - c >= 0:
+                    dp[coin] = min(dp[coin], dp[coin - c])
+            dp[coin] += 1
+        
+        if dp[-1] < float('inf'):
+            return dp[-1]
         else:
             return -1
+        
