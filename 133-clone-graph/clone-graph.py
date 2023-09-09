d5efc1d0b5b3b9e828f4597004b2b0606decdf37
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 20:10:42 -0700

    Time: 28 ms (83.51%) | Memory: 13.7 MB (38.40%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,18 +9,18 @@ class Solution(object):
         head = Node(node.val)
         queue = deque()
         visited = {}
-        queue.append((head.val, node, head))
+        queue.append((node, head))
         visited[head.val] = head
         while queue:
             for _ in range(len(queue)):
-                _, curr, clone_curr = queue.popleft()
+                curr, clone_curr = queue.popleft()
                 for neighbor in curr.neighbors:
                     if neighbor.val in visited:
                         clone_curr.neighbors.append(visited[neighbor.val])
                         continue
                     clone_neighbor = Node(neighbor.val)
                     clone_curr.neighbors.append(clone_neighbor)
-                    queue.append((neighbor.val, neighbor, clone_neighbor))
+                    queue.append((neighbor, clone_neighbor))
                     visited[neighbor.val] = clone_neighbor
         
         return head
