a7c3ee8d9077596f8523adc7fca251aba50c65fc
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-08 19:27:42 -0700

    Time: 3 ms (99.72%) | Memory: 13.1 MB (97.15%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,26 +1,14 @@
 class Solution(object):
-    def helper(self, n, ways):
-        if n in ways:
-            return ways[n]
-        
-        ways[n] = 0
-        if n - 1 in ways:
-            ways[n] += ways[n - 1]
-        else:
-            ways[n] += self.helper(n - 1, ways)
-        
-        if n - 2 in ways:
-            ways[n] += ways[n - 2]
-        else:
-            ways[n] += self.helper(n - 2, ways)
-        
-        return ways[n]
-    
     def climbStairs(self, n):
-        if n <= 3:
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
             return n
-        ways = {}
-        ways[1] = 1
-        ways[2] = 2
-        ways[3] = 3
-        return self.helper(n, ways)
+        i = 3
+        dp = [1, 2]
+        while (i <= n):
+            dp[0], dp[1] = dp[1], dp[0] + dp[1]
+            i += 1
+        return dp[-1]
