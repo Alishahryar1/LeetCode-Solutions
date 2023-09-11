b5a091be8109e8a03f41a3fb3c1ab5a9f9d6e76d
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-10 18:27:06 -0700

    Time: 13 ms (83.49%) | Memory: 13.4 MB (89.19%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,6 +4,7 @@ class Solution(object):
         if i == len(nums):
             output.append(subset)
             return
+            
         self.helper(nums, output, i + 1, subset + [nums[i]])
         self.helper(nums, output, i + 1, subset)
 
