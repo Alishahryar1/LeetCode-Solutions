d25a243f48e05f537828dcb7ddd0562ab07ff836
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-10 17:51:54 -0700

    Time: 63 ms (92.21%) | Memory: 17.2 MB (78.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,4 @@
 class Solution(object):
-
-    def index(self, letter):
-        return ord(letter) - 97
-
-    def sort(self, string):
-        counts = [0] * 26
-        for letter in string:
-            counts[self.index(letter)] += 1
-        sorted_string = ""
-        for i, count in enumerate(counts):
-            sorted_string += chr(i + 97) * count
-        return sorted_string
-    
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
@@ -20,7 +7,7 @@ class Solution(object):
         hashmap = {}
         hashcodes = []
         for string in strs:
-            sorted_string = self.sort(string)
+            sorted_string = ''.join(sorted(string))
             if sorted_string not in hashmap:
                 hashmap[sorted_string] = []
             hashmap[sorted_string].append(string)
