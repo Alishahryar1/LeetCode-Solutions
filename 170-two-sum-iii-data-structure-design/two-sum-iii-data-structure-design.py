e2679f8eaa3c76ae1fcc97e1eb80979296be783c
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-10 21:56:27 -0700

    Time: 1367 ms (23.19%) | Memory: 18.2 MB (15.94%) - LeetSync

--- 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
+++ 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
@@ -1,36 +1,30 @@
 class TwoSum(object):
 
     def __init__(self):
-        self.nums = []
-        self.length = 0
+        self.num_counts = {}
 
     def add(self, number):
         """
         :type number: int
         :rtype: None
         """
-        self.nums.append(number)
-        self.length += 1
-        i = self.length - 1
-        while (i - 1 >= 0 and self.nums[i - 1] > self.nums[i]):
-            self.nums[i - 1], self.nums[i] = self.nums[i], self.nums[i - 1]
-            i -= 1
+        if number not in self.num_counts:
+            self.num_counts[number] = 0
+        self.num_counts[number] += 1
 
     def find(self, value):
         """
         :type value: int
         :rtype: bool
         """
-        L = 0
-        R = self.length - 1
-        while L < R:
-            total = self.nums[L] + self.nums[R]
-            if total < value:
-                L += 1
-            elif total > value:
-                R -= 1
-            else:
-                return True
+        for num, count in self.num_counts.items():
+            diff = value - num
+            if diff in self.num_counts:
+                if diff == num and self.num_counts[diff] > 1:
+                    return True
+                elif diff != num:
+                    return True
+
         return False
         
 # Your TwoSum object will be instantiated and called as such:
