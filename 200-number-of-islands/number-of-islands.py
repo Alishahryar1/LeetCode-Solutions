76a070907925c3576e92d3a016fa0357e3e6a31e
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-09-11 18:41:34 -0700

    Time: 243 ms (53.89%) | Memory: 28.7 MB (69.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,12 +1,11 @@
 class Solution(object):
-    def visit(self, r, c, grid, rows, cols):
+    def visit(self, r, c, grid, rows, cols, neighbors):
         if min(r, c) < 0 or r == rows or c == cols or grid[r][c] == "0":
             return
         grid[r][c] = "0"
-        self.visit(r + 1, c, grid, rows, cols)
-        self.visit(r - 1, c, grid, rows, cols)
-        self.visit(r, c + 1, grid, rows, cols)
-        self.visit(r, c - 1, grid, rows, cols)
+
+        for dr, dc in neighbors:
+            self.visit(r + dr, c + dc, grid, rows, cols, neighbors)
     
     def numIslands(self, grid):
         """
@@ -15,10 +14,11 @@ class Solution(object):
         """
         rows, cols = len(grid), len(grid[0])
         islands = 0
+        neighbors = [[1,0],[-1,0],[0,1],[0,-1]]
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
                     islands += 1
-                    self.visit(r, c, grid, rows, cols)
+                    self.visit(r, c, grid, rows, cols, neighbors)
 
         return islands
