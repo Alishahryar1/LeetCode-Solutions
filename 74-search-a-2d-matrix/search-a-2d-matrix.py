9ac3261918a9a53b5e69e19e62c9058aae6dd725
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-10-27 06:31:19 -0700

    Time: 28 ms (30.65%) | Memory: 13.6 MB (48.90%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -7,28 +7,23 @@ class Solution(object):
         """
         L = 0
         R = len(matrix) - 1
-        while (L < R):
-            m = (L + R)//2
-            if target > matrix[m][0]:
-                if target <= matrix[m][-1]:
-                    L = m
-                    break
-                else:
-                    L = m + 1
-            elif target < matrix[m][0]:
-                R = m - 1
-            else:
-                return True
-        j = L
-        L = 0
-        R = len(matrix[j]) - 1
         while (L <= R):
             m = (L + R)//2
-            if target > matrix[j][m]:
-                L = m + 1
-            elif target < matrix[j][m]:
+            if target < matrix[m][0]:
                 R = m - 1
+            elif target > matrix[m][-1]:
+                L = m + 1
+            else:
+                break
+        r = m
+        L = 0
+        R = len(matrix[m]) - 1
+        while (L <= R):
+            m = (L + R)//2
+            if target < matrix[r][m]:
+                R = m - 1
+            elif target > matrix[r][m]:
+                L = m + 1
             else:
                 return True
-        
         return False
