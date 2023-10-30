cce4138a950885ab21328732c4faced40e3b2b3a
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-10-30 00:40:15 -0700

    Time: 159 ms (31.57%) | Memory: 24.7 MB (60.84%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,4 +1,10 @@
 class Solution(object):
+    def reverse(self, L, R, arr):
+        while (L < R):
+            arr[L], arr[R] = arr[R], arr[L]
+            L += 1
+            R -= 1
+    
     def rotate(self, nums, k):
         """
         :type nums: List[int]
@@ -6,10 +12,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         k %= len(nums)
-        temp = nums[len(nums) - k:]
-        i = len(nums) - k - 1
-        while (i >= 0):
-            nums[i + k] = nums[i]
-            i -= 1
-        for i in range(k):
-            nums[i] = temp[i]
+        self.reverse(0, len(nums) - 1, nums)
+        self.reverse(0, k - 1, nums)
+        self.reverse(k, len(nums) - 1, nums)
+        
