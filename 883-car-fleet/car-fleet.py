9b5205a149b13e83297fe514081861248e5012f2
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-13 16:22:20 -0800

    Time: 832 ms (33.38%) | Memory: 45 MB (5.47%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -20,12 +20,12 @@ class Solution(object):
         def time(car):
             return (target - car[0])/car[1]
 
-        while (i >= 0):
+        for i in reversed(range(len(cars))):
             stack.append(cars[i])
             if len(stack) >= 2:
                 carAhead = stack[-2]
                 carBehind = stack[-1]
                 if time(carAhead) >= time(carBehind):
                     stack.pop() 
-            i -= 1
+        
         return len(stack)
