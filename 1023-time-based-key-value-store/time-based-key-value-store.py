f96c6640976f16a7dc812b25b556623fbfbbaa8e
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-14 20:21:24 -0800

    Time: 653 ms (16.99%) | Memory: 72.5 MB (68.33%) - LeetSync

---
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -0,0 +1,54 @@
+class TimeMap(object):
+
+    def __init__(self):
+        self.hashMap = {}
+
+    def set(self, key, value, timestamp):
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        if key not in self.hashMap:
+            self.hashMap[key] = []
+        self.hashMap[key].append((value, timestamp))
+        
+
+    def get(self, key, timestamp):
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        if key not in self.hashMap:
+            return ""
+        n = len(self.hashMap[key])
+        L, R = 0, n - 1
+        maxtimestamp = 0
+        i = -1
+        while(L <= R):
+            m = (L + R)//2
+            _, currtimestamp = self.hashMap[key][m]
+            if currtimestamp <= timestamp and currtimestamp > maxtimestamp:
+                maxtimestamp = currtimestamp
+                i = m
+            if currtimestamp < timestamp:
+                L = m + 1
+            elif timestamp < currtimestamp:
+                R = m - 1
+            else:
+                maxtimestamp = currtimestamp
+                i = m
+                break
+        if i >= 0:
+            return self.hashMap[key][i][0]
+        else:
+            return ""
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
