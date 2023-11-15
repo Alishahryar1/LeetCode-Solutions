4e0e3043c69eb5bed08aaaf751e46870b79fabae
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-14 17:04:08 -0800

    Time: 28 ms (31.77%) | Memory: 13.6 MB (38.23%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -14,6 +14,7 @@ class Solution(object):
                 L = m + 1
             else:
                 break
+        
         r, L, R = m, 0, len(matrix[m]) - 1
         while (L <= R):
             m = (L + R)//2
@@ -23,4 +24,5 @@ class Solution(object):
                 L = m + 1
             else:
                 return True
+        
         return False
