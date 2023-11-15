a0a5f5db4c36d12dc6a409061c7f325dd355248f
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-14 17:44:16 -0800

    Time: 83 ms (74.47%) | Memory: 17.8 MB (11.06%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -5,54 +5,19 @@ class KthLargest(object):
         :type k: int
         :type nums: List[int]
         """
-        self.heap = [float('-inf')]
         self.k = k
+        self.heap = []
         for num in nums:
             self.add(num)
-        
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        if len(self.heap) < self.k + 1:
-            self.heap.append(val)
-            i  = len(self.heap) - 1
-            while self.heap[i // 2] > self.heap[i]:
-                self.heap[i // 2], self.heap[i] = self.heap[i], self.heap[i // 2]
-                i /= 2
-            return self.heap[1]
-        
-        if val > self.heap[1]: 
-            if len(self.heap) == 2:
-                self.heap[1] = val
-            else:
-                self.heap[1] = self.heap.pop()
-                i = 1
-                while 2 * i < len(self.heap):
-                    if (2 * i + 1 < len(self.heap) and 
-                    self.heap[2 * i + 1] < self.heap[2 * i] and 
-                    self.heap[i] > self.heap[2 * i + 1]):
-                        # Swap right child
-                        tmp = self.heap[i]
-                        self.heap[i] = self.heap[2 * i + 1]
-                        self.heap[2 * i + 1] = tmp
-                        i = 2 * i + 1
-                    elif self.heap[i] > self.heap[2 * i]:
-                        # Swap left child
-                        tmp = self.heap[i]
-                        self.heap[i] = self.heap[2 * i]
-                        self.heap[2 * i] = tmp
-                        i = 2 * i
-                    else:
-                        break
-                self.add(val)
-                    
-        return self.heap[1]
-
-            
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        else:
+            if val > self.heap[0]:
+                heapq.heappushpop(self.heap, val)
+        return self.heap[0]
