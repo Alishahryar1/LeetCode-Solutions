6d798c70bc175d0cfd80b48cf277943fd3ab04a9
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-17 01:18:19 -0800

    Time: 124 ms (7.83%) | Memory: 13.6 MB (48.70%) - LeetSync

---
+++ 311-sparse-matrix-multiplication/sparse-matrix-multiplication.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, mat1, mat2):
+        """
+        :type mat1: List[List[int]]
+        :type mat2: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                for k in range(len(mat1[0])):
+                    res[i][j] += mat1[i][k] * mat2[k][j]
+        return res
