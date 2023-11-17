5fcebc2ce1bbbe1dbbc20dfd86ec588b4e86bf7e
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-16 18:53:29 -0800

    Time: 75 ms (62.91%) | Memory: 14.6 MB (9.08%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        if len(p) > len(s):
+            return []
+        def index(char):
+            return ord(char) - ord('a')
+        
+        counts1 = [0] * 26
+        counts2 = [0] * 26
+        res = []
+        for i in range(len(p)):
+            char = p[i]
+            counts1[index(char)] += 1
+            char = s[i]
+            counts2[index(char)] += 1
+        
+        for i in range(len(p), len(s)):
+            if counts1 == counts2:
+                res.append(i - len(p))
+            char = s[i]
+            counts2[index(char)] += 1
+            char = s[i - len(p)]
+            counts2[index(char)] -= 1
+
+        if counts1 == counts2:
+            res.append(i + 1 - len(p))  
+        
+        return res
