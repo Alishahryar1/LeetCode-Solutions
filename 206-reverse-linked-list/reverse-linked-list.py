6a74320761f8871264f2ced7fad64c409f416791
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-17 00:25:31 -0800

    Time: 11 ms (94.21%) | Memory: 15.3 MB (38.63%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -4,20 +4,16 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def helper(self, prev, curr):
-        if curr.next == None:
-            curr.next = prev
-            return curr
-        temp = curr.next
-        curr.next = prev
-        return self.helper(curr, temp)
-        
-
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        if head == None:
-            return head
-        return self.helper(None, head)
+        prev = None
+        curr = head
+        while (curr != None):
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
