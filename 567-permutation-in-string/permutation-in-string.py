465d0b4012d9564722f009460fd2fabed1bf4dea
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-16 18:48:47 -0800

    Time: 35 ms (86.87%) | Memory: 13.8 MB (13.07%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -7,21 +7,24 @@ class Solution(object):
         """
         if len(s1) > len(s2):
             return False
+
+        def index(char):
+            return ord(char) - ord('a')
         
         counts1 = [0] * 26
         counts2 = [0] * 26
         for i in range(len(s1)):
             char = s1[i]
-            counts1[ord(char) - ord('a')] += 1
+            counts1[index(char)] += 1
             char = s2[i]
-            counts2[ord(char) - ord('a')] += 1
+            counts2[index(char)] += 1
         
         for i in range(len(s1), len(s2)):
             if counts1 == counts2:
                 return True
             char = s2[i]
-            counts2[ord(char) - ord('a')] += 1
+            counts2[index(char)] += 1
             char = s2[i - len(s1)]
-            counts2[ord(char) - ord('a')] -= 1
+            counts2[index(char)] -= 1
             
         return counts1 == counts2
