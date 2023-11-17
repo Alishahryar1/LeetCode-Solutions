4a0d6cf8b5815ea3e06da886078d863d1cbedd5c
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-16 18:07:06 -0800

    Time: 57 ms (85.60%) | Memory: 13.6 MB (49.55%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -8,11 +8,13 @@ class Solution(object):
         L = R = 0
         counts = [0] * 26
         maxlen = 0
+        maxf = 0
         while (R < len(s)):
             index = ord(s[R]) - ord('A')
             counts[index] += 1
+            maxf = max(maxf, counts[index])
             length = R - L + 1
-            replace = length - max(counts)
+            replace = length - maxf
             if replace <= k:
                 maxlen = max(maxlen, length)
             else:
@@ -21,7 +23,7 @@ class Solution(object):
                     counts[index] -= 1
                     L += 1
                     length = R - L + 1
-                    replace = length - max(counts)
+                    replace = length - maxf
             
             R += 1
         return maxlen
