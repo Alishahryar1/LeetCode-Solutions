86d0e3d24e4cedc94ce29039ca5c51c27b62ad9c
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-16 18:53:49 -0800

    Time: 71 ms (71.83%) | Memory: 14.5 MB (19.72%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -12,13 +12,13 @@ class Solution(object):
         
         counts1 = [0] * 26
         counts2 = [0] * 26
-        res = []
         for i in range(len(p)):
             char = p[i]
             counts1[index(char)] += 1
             char = s[i]
             counts2[index(char)] += 1
         
+        res = []
         for i in range(len(p), len(s)):
             if counts1 == counts2:
                 res.append(i - len(p))
