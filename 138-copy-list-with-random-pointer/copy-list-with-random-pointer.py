42ffc9661667b95a1c0df887f69067ef78340963
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-17 08:57:40 -0800

    Time: 30 ms (81.37%) | Memory: 13.8 MB (78.18%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -0,0 +1,50 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def insert(self, head, tail, node):
+        if tail == None:
+            head = tail = node
+        else:
+            tail.next = node
+            tail = tail.next
+        return head, tail
+
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        curr = head
+        while (curr != None):
+            newNode = Node(curr.val)
+            temp = curr.next
+            curr.next = newNode
+            newNode.next = temp
+            curr = curr.next.next
+        
+
+        curr = head
+        while (curr != None):
+            curr.next.random = curr.random.next if (curr.random != None) else None
+            curr = curr.next.next
+        
+        head1 = tail1 = None
+        curr = head
+        while (curr != None):
+            node = curr.next
+            curr.next = curr.next.next
+            node.next = None
+            head1, tail1 = self.insert(head1, tail1, node)
+            curr = curr.next
+        return head1
+
+        return None
+
+        
