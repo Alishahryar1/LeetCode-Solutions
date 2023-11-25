1d6400fce0d12cd75ba2ef1aecf48331b8232885
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2023-11-25 01:57:27 -0800

    Time: 16 ms (93.49%) | Memory: 14.4 MB (63.27%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,26 +1,18 @@
 class Solution(object):
+    def transform(self, string):
+        letterMap = {}
+        res = []
+        for i, s in enumerate(string):
+            if s not in letterMap:
+                letterMap[s] = i
+            res.append(letterMap[s])
+        return res
+
     def isIsomorphic(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
-        List1 = []
-        List2 = []
-        for i in range(265):
-            List1.append([])
-            List2.append([])
-        for i in range(len(s)):
-            List1[ord(s[i])].append(i)
-        for i in range(len(t)):
-            List2[ord(t[i])].append(i)
-        for i in range(len(List1)):
-            if List1[i] == []:
-                continue
-            for j in range(len(List2)):
-                if List1[i] == List2[j]:
-                    List2[j] = []
-                    break
-            else:
-                return False
-        return True
+        List1, List2 = self.transform(s), self.transform(t)
+        return List1 == List2
