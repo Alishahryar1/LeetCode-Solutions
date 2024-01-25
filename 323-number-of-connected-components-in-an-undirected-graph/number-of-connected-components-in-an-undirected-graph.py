152784a88b88af0fb68e73de32f435f28a04a70f
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2024-01-24 22:34:14 -0800

    Time: 75 ms (33.41%) | Memory: 13 MB (97.14%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,40 @@
+class UnionFind():
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [0 for _ in range(n)]
+    
+    def find(self, n):
+        p = self.par[n]
+        while (p != self.par[p]):
+            #path compression
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.par[p1] = p2
+        else:
+            self.par[p2] = p1
+            self.rank[p1] += 1
+        return True
+
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        uf = UnionFind(n)
+        for a, b in edges:
+            uf.union(a, b)
+        ans = set()
+        for i, _ in enumerate(uf.par):
+            ans.add(uf.find(i))
+        return len(ans)
