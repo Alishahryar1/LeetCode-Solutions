0578c5d1b015df64b14f0c6c99a1ecc5c6b56d8e
Ali Shahryar Khokhar <alishahryar2@gmail.com>
2024-03-02 21:45:34 -0800

    Time: 43 ms (43.39%) | Memory: 11.8 MB (62.91%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,12 +1,14 @@
 class Solution(object):
-    def helper(self, candidates, target, output, subset, i):
-        if target < 0:
+    def helper(self, i, candidates, target, stack, res):
+        if target < 0 or i == len(candidates):
             return
         if target == 0:
-            output.append(subset)
-            return
-        for j in range(i, len(candidates)):
-            self.helper(candidates, target - candidates[j], output, subset + [candidates[j]], j)
+            res.append(stack[:])
+            return 
+        self.helper(i + 1, candidates, target, stack, res)
+        stack.append(candidates[i])
+        self.helper(i, candidates, target - candidates[i], stack, res)
+        stack.pop()
     
     def combinationSum(self, candidates, target):
         """
@@ -14,6 +16,6 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        output = []
-        self.helper(candidates, target, output, [], 0)
-        return output
+        res = []
+        self.helper(0, candidates, target, [], res)
+        return res
